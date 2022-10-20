63abdba114cc793ae615c92fcf65259c6c955111
Kacperski81 <99400249+Kacperski81@users.noreply.github.com>
2022-10-20 12:56:50 +0100

    first commit

---
+++ precache-manifest.fdc52d538fc1e3798f378fa4630976b0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529c197f8ec09825e591cd389daee8f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c85031bae0b64f01003",
+    "url": "/static/css/2.68f0c4ae.chunk.css"
+  },
+  {
+    "revision": "6c85031bae0b64f01003",
+    "url": "/static/js/2.bad7d153.chunk.js"
+  },
+  {
+    "revision": "e8ccc7cf4c855a56e3a7079a2b405db4",
+    "url": "/static/js/2.bad7d153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d206d05d141106ca2046",
+    "url": "/static/js/3.3a694209.chunk.js"
+  },
+  {
+    "revision": "068ef50cd0f2e14d4240",
+    "url": "/static/js/main.c0a86a38.chunk.js"
+  },
+  {
+    "revision": "55b71698956bea49cdd5",
+    "url": "/static/js/runtime-main.01e98d6a.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
